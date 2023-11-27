628bc606e41eddef88c4dde1cf091055069f38fe
Chris Cox <chris.cox@abandonedfactory.net>
2023-11-26 19:47:03 -0800

    Fixing logic to auto-create database

--- pkg/repositories/utilities/migrate.go
+++ pkg/repositories/utilities/migrate.go
@@ -12,9 +12,14 @@ import (
 
 func ConnectAndMigratePostgres(connectFmt string, targetDbName string, migrationsFS embed.FS) (*sql.DB, error) {
 	// Attempt to connect to the project DB
-	projectDb, err := sql.Open("postgres", fmt.Sprintf(connectFmt, targetDbName))
+	projectConnStr := fmt.Sprintf(connectFmt, targetDbName)
+	projectDb, err := sql.Open("postgres", projectConnStr)
+	if err != nil {
+		return nil, fmt.Errorf("connecting to project db: %w", err)
+	}
+	err = projectDb.Ping()
 	if err == nil {
-		slog.Debug("Successfully connected to exiting project DB", "db", targetDbName)
+		slog.Debug("Successfully connected to exiting project DB", "conn", projectConnStr, "db", targetDbName)
 		err = MigrateSql(projectDb, migrationsFS)
 		return projectDb, err
 	}
@@ -22,21 +27,25 @@ func ConnectAndMigratePostgres(connectFmt string, targetDbName string, migration
 	// If we failed to connect to the project DB, try
 	// connecting to the system DB and creating the DB
 	// TODO: inspect the error to see if this is worth trying
-	slog.Error("Failed to connect to project DB", "err", err)
+	slog.Debug("Failed to connect to project DB", "err", err)
 
 	// connect to master DB and ensure the DB exists
 	rootDb, err := sql.Open("postgres", fmt.Sprintf(connectFmt, "postgres"))
 	defer rootDb.Close()
 	if err != nil {
 		return nil, fmt.Errorf("connect to root db: %w", err)
+	} else {
+		slog.Debug("Connected to root DB")
 	}
-	_, err = rootDb.Exec(fmt.Sprintf(`CREATE DATABASE IF NOT EXISTS %s`, targetDbName))
+	_, err = rootDb.Exec(fmt.Sprintf(`CREATE DATABASE %s`, targetDbName))
 	if err != nil {
 		return nil, fmt.Errorf("creating project db: %w", err)
+	} else {
+		slog.Debug("Created project db", "db", targetDbName)
 	}
 
 	// connect to the target DB and run migrations
-	projectDb, err = sql.Open("postgres", fmt.Sprintf(connectFmt, targetDbName))
+	projectDb, err = sql.Open("postgres", projectConnStr)
 	if err != nil {
 		slog.Error("Still unable to connect to project DB", "err", err)
 		return nil, err
